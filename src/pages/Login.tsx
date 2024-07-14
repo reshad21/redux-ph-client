ec8e0b2afa055513324144f76522104826bfc1b5
reshad21 <miakala7@gmail.com>
2024-07-14 23:50:26 +0600

    create now academic semister

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,28 +1,33 @@
-import { Button } from "antd";
-import { FieldValues, useForm } from "react-hook-form";
+import { Button, Row } from "antd";
+import { FieldValues } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import PHForm from "../components/form/PHForm";
+import PHInput from "../components/form/PHInput";
 import { useLoginMutation } from "../redux/features/auth/authApi";
-import { setUser, TUser } from "../redux/features/auth/authSlice";
+import { TUser, setUser } from "../redux/features/auth/authSlice";
 import { useAppDispatch } from "../redux/hooks";
 import { verifyToken } from "../utils/verifyToken";
 
 const Login = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const { register, handleSubmit } = useForm({
-    defaultValues: {
-      userId: "0001",
-      password: "admin12345",
-    },
-  });
+  // const { register, handleSubmit } = useForm({
+  //   defaultValues: {
+  //     userId: '0001',
+  //     password: 'admin12345',
+  //   },
+  // });
 
-  const [login, { data, error }] = useLoginMutation();
+  const defaultValues = {
+    userId: "0001",
+    password: "admin12345",
+  };
 
-  console.log("data =>", data);
-  console.log("data =>", error);
+  const [login] = useLoginMutation();
 
   const onSubmit = async (data: FieldValues) => {
+    console.log(data);
     const toastId = toast.loading("Logging in");
 
     try {
@@ -32,27 +37,22 @@ const Login = () => {
       };
       const res = await login(userInfo).unwrap();
       const user = verifyToken(res.data.accessToken) as TUser;
-
       dispatch(setUser({ user: user, token: res.data.accessToken }));
-      toast.success("Logged in successfully", { id: toastId, duration: 2000 });
-      navigate(`/${user?.role}/dashboard`);
+      toast.success("Logged in", { id: toastId, duration: 2000 });
+      navigate(`/${user.role}/dashboard`);
     } catch (err) {
-      toast.error("something went wrong!", { id: toastId, duration: 2000 });
+      toast.error("Something went wrong", { id: toastId, duration: 2000 });
     }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="id">ID: </label>
-        <input type="text" id="id" {...register("userId")} />
-      </div>
-      <div>
-        <label htmlFor="password">Password: </label>
-        <input type="text" id="password" {...register("password")} />
-      </div>
-      <Button htmlType="submit">Login</Button>
-    </form>
+    <Row justify="center" align="middle" style={{ height: "100vh" }}>
+      <PHForm onSubmit={onSubmit} defaultValues={defaultValues}>
+        <PHInput type="text" name="userId" label="ID:" />
+        <PHInput type="text" name="password" label="Password" />
+        <Button htmlType="submit">Login</Button>
+      </PHForm>
+    </Row>
   );
 };
 
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,15 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { RootState } from "../store";
+
+
+import {
+    BaseQueryApi,
+    BaseQueryFn,
+    DefinitionType,
+    FetchArgs,
+    createApi,
+    fetchBaseQuery,
+} from '@reduxjs/toolkit/query/react';
+import { logout, setUser } from '../features/auth/authSlice';
+import { RootState } from '../store';
 
 const baseQuery = fetchBaseQuery({
     baseUrl: 'http://localhost:5000/api/v1',
@@ -15,9 +25,45 @@ const baseQuery = fetchBaseQuery({
     },
 })
 
+const baseQueryWithRefreshToken: BaseQueryFn<
+    FetchArgs,
+    BaseQueryApi,
+    DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+    let result = await baseQuery(args, api, extraOptions);
+
+    if (result?.error?.status === 401) {
+        //* Send Refresh
+        console.log('Sending refresh token');
+
+        const res = await fetch('http://localhost:5000/api/v1/auth/refresh-token', {
+            method: 'POST',
+            credentials: 'include',
+        });
+
+        const data = await res.json();
+
+        if (data?.data?.accessToken) {
+            const user = (api.getState() as RootState).auth.user;
+
+            api.dispatch(
+                setUser({
+                    user,
+                    token: data.data.accessToken,
+                })
+            );
+
+            result = await baseQuery(args, api, extraOptions);
+        } else {
+            api.dispatch(logout());
+        }
+    }
+
+    return result;
+};
+
 export const baseApi = createApi({
     reducerPath: 'baseApi',
-    baseQuery: baseQuery,
-
+    baseQuery: baseQueryWithRefreshToken,
     endpoints: () => ({}),
 });
